935341032c8684c5be3dfd4b4a94be8c89e2b15b
StevenUM21 <suruchimam@unemi.edu.ec>
2025-08-06 17:10:26 -0500

    Se agrego funcion de obtencion de preguntas y guardado de respuestas de encuesta

--- preguntas.js
+++ preguntas.js
@@ -5,9 +5,11 @@ let btnSave = document.getElementById("btn-save")
 btnSave.addEventListener("click", async function(e){
     e.preventDefault()
     
-    let form = document.getElementById("formularioEncuesta")
-    const data = new URLSearchParams(new FormData(form));
-    const response = await fetch("https://script.google.com/macros/s/AKfycbwD0wMCay4Lp0NUiaM63dGMPolvuTiGm6k6F0e30XU-sr0RHmYx0l2qzN5t0svQ-o5B/exec", {
+    let form = document.getElementById("formularioEncuesta");
+    const formData = new FormData(form);
+    formData.append("accion", "registrarpregunta"); 
+    const data = new URLSearchParams(formData);
+    const response = await fetch("https://script.google.com/macros/s/AKfycbzaqAw4xv2DlqircRl2giG25VX7HdOhtXSKjVcmaeoobHaQKnbl2hbVE16SOVwRWZtA/exec", {
         "method": "POST",
         "body": data,
     })
